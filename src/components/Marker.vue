36e0fa2f1e7223778025382c74a8aaee5ed2e480
brandonxiang <1542453460@qq.com>
2017-03-15 00:07:47 +0800

    fix marker image bug

--- src/components/Marker.vue
+++ src/components/Marker.vue
@@ -4,10 +4,19 @@
 <script>
 import L from 'leaflet';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 export default {
   props: ['position', 'opacity', 'title'],
   mounted() {
-    L.Icon.Default.imagePath = '../../node_modules/leaflet/dist/images/';
+    const DefaultIcon = L.icon({
+      iconUrl: icon,
+      shadowUrl: iconShadow,
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
+
     const options = {
       opacity: this.opacity,
       title: this.title,
